d5dfd7bd5c67762aae2c186734e127d32bce9772
cristhiantoala <72051136+cristhiantoala@users.noreply.github.com>
2022-01-24 00:17:06 -0500

    Update insertar.php

--- servidor/insertar.php
+++ servidor/insertar.php
@@ -16,24 +16,8 @@ $teclas 		= $_POST['tecla'];
 $res 	= $_POST['respuestas'];
 	
 
-
-	// si no hay errores registramos al noticia
-		// inserto los datos de registro en la db
-<<<<<<< HEAD
-		$query  = "INSERT INTO `data` (mouse,teclado,emocion) VALUES ('$coordenadas','$teclas','$res')";
-		echo "hola";
-		
-		$result = mysqli_query($dbConn,$query);
-		echo "aaaaaaaaaaaa";
-		die;
-		/*$query  = "INSERT INTO `data` (teclado) VALUES ('$teclas')";
-		$result = mysqli_query($dbConn,$query);
-		$query  = "INSERT INTO `data` (emocion) VALUES ('$res')";
-		$result = mysqli_query($dbConn,$query);*/
-=======
 		$query  = "INSERT INTO `test` (mouse,teclado,emocion) VALUES ('$coordenadas','$teclas','$res')";
 		$result = mysqli_query($dbConn,$query);
->>>>>>> 683d8f7e47d999dec28694a619640400fe4789e6
 		header("Location: ../");
 		die;
 	
